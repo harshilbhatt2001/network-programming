09348348d0eeb1743195d7aa45bd47ccfc9d53f8
Harshil_Bhatt <harshilbhatt2001@gmail.com>
2020-05-02 00:10:37 +0530

    multi-threaded file transfer

--- File-Transfer/client.py
+++ File-Transfer/client.py
@@ -5,25 +5,31 @@ Created on Fri May 01 23:13:22 2020
 
 
 import socket
+from threading import Thread
+from socketserver import ThreadingMixIn
 
-host = socket.gethostname()
-port = 60000
-s = socket.socket()
 
-s.connect((host, port))
-s.send(b'Hello Server!')
+TCP_IP = 'localhost'
+TCP_PORT = 9001
+BUFFER_SIZE = 1024
 
-with open('received_file', 'wb') as f:
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect((TCP_IP, TCP_PORT))
+
+filename = str(input("File to be saved as: "))
+
+with open(filename, 'wb') as f:
     print("File opened")
     while True:
-        print("Receiving data...")
-        data = s.recv(20480)
+        data = s.recv(BUFFER_SIZE)
         print("data = %s" % data)
         if not data:
+            f.close()
+            print("File closed")
             break
         f.write(data)
 
-f.close()
-print("Successfully got the file")
+print("Successfully recieved the file")
 s.close()
-print("Connection closed")
+print("Connection Closed")
+        
--- File-Transfer/server.py
+++ File-Transfer/server.py
@@ -5,29 +5,50 @@ Created on Fri May 01 23:13:22 2020
 
 
 import socket
+from threading import Thread
+from socketserver import ThreadingMixIn
 
-port = 60000
-s = socket.socket()
-host = socket.gethostname()
-s.bind((host, port))
-s.listen()
 
-print("Server Listening...")
+TCP_IP = 'localhost'
+TCP_PORT = 9001
+BUFFER_SIZE = 1024
+
+class ClientThread(Thread):
+
+    def __init__(self, ip, port, sock):
+        Thread.__init__(self)
+        self.ip = ip
+        self.port = port
+        self.sock = sock
+        print("New thread started for " + ip + ": " + str(port))
+    
+    def run(self):
+        filename = 'intro.txt'
+        f = open(filename, 'rb')
+        while True:
+            l = f.read(BUFFER_SIZE)
+            while l:
+                self.sock.send(l)
+                l = f.read(BUFFER_SIZE)
+            if not l:
+                f.close()
+                self.sock.close()
+                break
+
+tcpsock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+tcpsock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+tcpsock.bind((TCP_IP, TCP_PORT))
+threads = []
 
 while True:
-    conn, addr = s.accept()
-    print("Got connection from ", addr)
-    data = conn.recv(20480)
+    tcpsock.listen()
+    print("Waiting for incoming connections...")
+    (conn, (ip, port)) = tcpsock.accept()
+    print("Got connection from ", (ip, port))
+    newthread = ClientThread(ip, port, conn)
+    newthread.start()
+    threads.append(newthread)
 
-    filename = 'intro.txt'
-    f = open(filename, 'rb')
-    l = f.read(20480)
-    while l:
-        conn.send(l)
-        print("Sent ",repr(l))
-        l = f.read(20480)
-    f.close()
-
-    print("Finished Sending")
-    conn.send(b'Thank you for connecting')
-    conn.close()
+for t in threads:
+    t.join()
+    
